8b2294a0565f9a15ea35b5ab0a0989c1063ce870
AryanKakade <153463913+aryankakadeGit@users.noreply.github.com>
2026-01-02 00:22:41 +0530

    Initial Upload

---
+++ Mapping.py
@@ -0,0 +1,4 @@
+Information = {"Name": "Rahul", "Age":25 ,"City":"Pune","Marks":89.90}
+print(type(Information))
+print(Information)
+print(Information["City"])
---
+++ Size.py
@@ -0,0 +1,8 @@
+import sys
+No=112345672345678234567 #int
+Marks=90.90 #float
+Name="Rahul" #str
+print(sys.getsizeof(No));
+print(sys.getsizeof(Marks));
+print(sys.getsizeof(Name));
+
