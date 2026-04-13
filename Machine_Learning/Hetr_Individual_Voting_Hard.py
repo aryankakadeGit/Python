f51bc5b9669f7934c95e0039cee559cbfbd3c209
AryanKakade <153463913+aryankakadeGit@users.noreply.github.com>
2026-04-13 22:32:34 +0530

    Initial Upload

---
+++ Machine_Learning/Hetr_Individual_Voting_Hard.py
@@ -0,0 +1,46 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.ensemble import VotingClassifier
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+
+# Step 1 : Load Dataset
+data = load_breast_cancer()
+
+X = data.data
+Y = data.target
+
+print("Shape of X : ",X.shape)
+print("Shape of Y : ",Y.shape)
+
+
+# Step 2 : Split the dataset
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=42)
+
+# Step 3 : Create Base Models
+model_lr = LogisticRegression(max_iter=5000)
+model_dt = DecisionTreeClassifier(random_state=42)
+model_knn = KNeighborsClassifier(n_neighbors=5)
+
+# Step 4 : Train Base Models
+model_lr.fit(X_train,Y_train)
+model_dt.fit(X_train,Y_train)
+model_knn.fit(X_train,Y_train)
+
+#Step 5 : Calculate individual accuracy
+Pred_lr = model_lr.predict(X_test)
+Pred_dt = model_dt.predict(X_test)
+Pred_knn = model_knn.predict(X_test)
+
+acc_lr = accuracy_score(Pred_lr,Y_test)
+acc_dt = accuracy_score(Pred_dt,Y_test)
+acc_knn = accuracy_score(Pred_knn,Y_test)
+
+print("Individual model accuracy : ")
+print("Logistic regression : ",acc_lr)
+print("Decision tree classifier : ",acc_dt)
+print("K nearest neighbour : ",acc_knn)
---
+++ Machine_Learning/Hetr_Voting_Soft.py
@@ -0,0 +1,46 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.ensemble import VotingClassifier
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+
+# Step 1 : Load Dataset
+data = load_breast_cancer()
+
+X = data.data
+Y = data.target
+
+print("Shape of X : ",X.shape)
+print("Shape of Y : ",Y.shape)
+
+
+# Step 2 : Split the dataset
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=42)
+
+# Step 3 : Create Base Models
+model_lr = LogisticRegression(max_iter=5000)
+model_dt = DecisionTreeClassifier(random_state=42)
+model_knn = KNeighborsClassifier(n_neighbors=5)
+
+# Step 4 : Train Base Models
+model_lr.fit(X_train,Y_train)
+model_dt.fit(X_train,Y_train)
+model_knn.fit(X_train,Y_train)
+
+# Step 5 : HArd Voting Classsification
+Soft_model = VotingClassifier(
+                            estimators=[
+                                            ('lr',model_lr),
+                                            ('dt',model_dt),
+                                            ('knn',model_knn)
+                                        ],
+                            voting="soft"
+                              )
+Soft_model.fit(X_train,Y_train)
+pred_Soft = Soft_model.predict(X_test)
+acc_Soft = accuracy_score(pred_Soft,Y_test)
+print("Soft Voting Accuracy : ",acc_Soft)
